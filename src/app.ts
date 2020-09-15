304fba033c0bbc0458b4c28f25e8606d70303092
yuiko.akiyoshi <yuiko.akiyoshi@yuikoakiyoshinoMacBook-Air.local>
2020-09-15 22:07:41 +0900

    getterの活用

--- src/app.ts
+++ src/app.ts
@@ -143,6 +143,14 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 class ProjectItem extends Component<HTMLUListElement,HTMLLIElement>{
     private project: Project;
 
+    get manday(){
+        if(this.project.manday < 20) {
+            return this.project.manday.toString() + '人日'
+        } else {
+            return (this.project.manday / 20).toString() + '人月'
+        }
+    }
+
     constructor(hostId: string, project: Project){
         super('single-project', hostId, false, project.id);   
         this.project = project; 
@@ -155,7 +163,7 @@ class ProjectItem extends Component<HTMLUListElement,HTMLLIElement>{
     }
     renderContent(){
         this.element.querySelector('h2')!.textContent = this.project.title;
-        this.element.querySelector('h3')!.textContent = this.project.manday.toString();
+        this.element.querySelector('h3')!.textContent = this.manday//getterはプロパティのように呼び出せる
         this.element.querySelector('p')!.textContent = this.project.description;
     }
 }
